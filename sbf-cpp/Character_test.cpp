2efe940300be7beda97ef0cd97320aa743e6eae2
Tom Hicks <headhunter3@gmail.com>
2023-04-25 11:31:44 -0700

    Cleans up empty character tests.

--- sbf-cpp/Character_test.cpp
+++ sbf-cpp/Character_test.cpp
@@ -7,16 +7,12 @@ using namespace SBF;
 using namespace Test;
 using namespace std;
 
-TestResults main_test_Character(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
   TestResults results;
 
+  results.skip("// TODO: SBF::Character::*");
+
   PrintResults(cout, results);
-
-  return results;
-}
-
-int main(int argc, char* argv[]) {
-  TestResults results = main_test_Character(argc, argv);
 
   return results.failed() + results.errors();
 }
